d6d30e77088fb070e58d04c0a50254448989e651
AYDE JUAREZ <aydejuarez@MacBook-Pro-de-AYDE-2.local>
2024-07-29 21:18:06 -0600

    Add a cloudbuild.yaml file

--- sniim/db/mongo.py
+++ sniim/db/mongo.py
@@ -1,13 +1,14 @@
 import os
-from urllib.parse import quote_plus
 from pymongo import MongoClient
 
 
 class Mongoclient:
-    def __init__(self, *args, db_collection=None):
+    def __init__(self, db_collection=None):
         self.host = os.environ.get('MONGO_HOST', '0.0.0.0')
-        self.port = os.environ.get('MONGO_PORT', '27017')
-        if os.environ.get('CONNECT_WITH_USER', 'False') == 'True':
+        self.port = int(os.environ.get('MONGO_PORT', '27017'))
+        self.connect_with_user = os.environ.get('CONNECT_WITH_USER', 'False') == 'True'
+
+        if self.connect_with_user:
             self.user = os.environ.get('MONGO_USER', 'central')
             self.password = os.environ.get('MONGO_PASSWORD', 'central')
 
@@ -16,15 +17,21 @@ class Mongoclient:
         self.db_collection = db_collection
 
         self.db = self.client[self.db_name]
-        self.collection = self.db[self.db_collection]
+        if self.db_collection:
+            self.collection = self.db[self.db_collection]
 
     @property
     def _connection_string(self):
-        if os.environ.get('CONNECT_WITH_USER', 'False') == 'True':
-            return "mongodb://{0}:{1}@{2}:{3}".format(self.user, self.password, self.host, self.port)
+        if self.connect_with_user:
+            # Using username/password authentication
+            return f"mongodb://{quote_plus(self.user)}:{quote_plus(self.password)}@{self.host}:{self.port}/?retryWrites=true&w=majority"
         else:
-            return "mongodb://{0}:{1}".format(self.host, self.port)
+            # Without username/password
+            return f"mongodb://{self.host}:{self.port}/?retryWrites=true&w=majority"
 
     def insert_one(self, document):
-        inserted = self.collection.insert_one(document)
-        return True if getattr(inserted, 'inserted_id') else False
+        if self.db_collection:
+            inserted = self.collection.insert_one(document)
+            return True if getattr(inserted, 'inserted_id') else False
+        else:
+            raise ValueError("Database collection not specified")
